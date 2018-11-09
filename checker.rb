6ade878b6ff7a1285732c142f4ee4a8ceb556b97
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-11-09 12:12:32 +0000

    Ignore debug/verbose output in checker.rb

--- checker.rb
+++ checker.rb
@@ -12,7 +12,7 @@ prev_digest = input_file[0].chars.each_slice(2).map(&:join).map { |b| b.to_i(16)
 target = input_file[1].to_i
 
 stdout, stderr = Open3.capture3("bash -c \"time ./0123HomeWork #{ARGV[0]} #{ARGV[1]} < #{ARGV[2]}\"")
-output = stdout.split("\n")
+output = stdout.split("\n").last(4)
 
 nusnet_id = output[0]
 cur_time = [output[1].to_i].pack('L>')
@@ -24,6 +24,8 @@ correct_digest = Digest::SHA256.hexdigest(input)
 
 to_compare = correct_digest[0..3].to_i(16)
 
+# puts "Input: #{input.bytes.map{ |b| b.to_s(16) }.join(", ")}"
+
 if correct_digest == digest && to_compare < target
   puts "Correct!"
 else
@@ -33,5 +35,6 @@ else
   puts "Correct digest: #{correct_digest}"
   puts "Given digest: #{digest}"
 end
+puts "-------------"
 puts stdout
 puts stderr
